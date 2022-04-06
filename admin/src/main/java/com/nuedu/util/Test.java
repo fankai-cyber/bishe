3ea38ebd2cb53cc9daa3c0960dffab5380bf0477
fankai-cyber <66164959+fankai-cyber@users.noreply.github.com>
2022-04-06 15:23:52 +0800

    毕业设计

--- admin/src/main/java/com/nuedu/util/Test.java
+++ admin/src/main/java/com/nuedu/util/Test.java
@@ -6,4 +6,9 @@ public class Test {
     public void method1(){
         this.name = "fankai";
     }
+
+    public static void main(String[] args) {
+        System.out.println(new StringBuilder(null) == null);
+
+    }
 }
